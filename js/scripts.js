212eaa570c7171ff2cdc38226dd06bd224746a06
samstoia <samstoia@gmail.com>
2019-01-24 22:32:32 -0800

    Add proper function for pig latin translation

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,20 @@
 // ### Business / Back-End Logic
-var vowels = ["a", "e", "i", "o", "u"]; //ok
-var phrase = function(word) {
-  if ( (word.length === 1) || (word[0] === "y") || (word[0] === "Y") ) {
-    return word + "ay";
-  } else if ( (word[0] === vowels[0]) || (word[0] === vowels[1]) || (word[0] === vowels[2]) || (word[0] === vowels[3]) || (word[0] === vowels[4]) ) {
-    return word + "way";
-  // } else if ( (word[0] !== vowels[0]) || (word[0] !== vowels[1]) || (word[0] !== vowels[2]) || (word[0] !== vowels[3]) || (word[0] !== vowels[4]) ) {
-  //   return "Maaaaaybe this will work"
-  }
-    else {
-    return word = "error";
-  };
-};
+function translatePigLatin(words) {
+    var firstVowel = words.match(/[aeiou]/);
+    var firstPosition = words.indexOf(firstVowel);
+      if (firstPosition > 0) {
+        return words.slice(firstPosition) + words.slice(0, firstPosition) + "ay";
+      }
+        return words + "way"
+}
+
+
 // ### User / Front-End Logic
 $(document).ready(function() { //ok
   $("form#formOne").submit(function(event) { //ok
     event.preventDefault(); //ok
     var words = $("input#input").val(); //ok
-    var word = words.split("");
-    var firstletter = word.indexOf(0);
-    var result = phrase(word.join(''));
-    $("p#outlet").text(result);
+    console.log(words);
+    $("p#outlet").text(translatePigLatin(words));
   });
 });
